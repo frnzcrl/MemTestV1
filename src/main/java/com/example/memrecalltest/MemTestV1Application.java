1264fc7368139115ec13b26042703a2d20f9b7f0
frnzcrl <frenzcarlasibuque97@gmail.com>
2021-07-15 19:57:36 +0800

    Updated application running config

--- src/main/java/com/example/memrecalltest/MemTestV1Application.java
+++ src/main/java/com/example/memrecalltest/MemTestV1Application.java
@@ -1,13 +1,19 @@
 package com.example.memrecalltest;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MemTestV1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(MemTestV1Application.class, args);
+		
+		//runs the project automatically in a browser
+		Runtime rt = Runtime.getRuntime();
+	    rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
 	}
 
 }
